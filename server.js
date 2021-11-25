a33be2f1f0954e349050331e17c201cbfd73b897
Shayan-Irfan <ishayan767@gmail.com>
2021-11-25 21:21:16 +0500

    Ready to deploy

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use('/', express.static(__dirname + '/static'));
 
